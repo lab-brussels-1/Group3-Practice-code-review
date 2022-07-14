79b95fd086c1d8aebc4a161c48e6b25d0b8c686f
Maria <naplesaries2014@gmail.com>
2022-07-14 17:41:22 +0200

    formated code to pass checks

--- src/intersection/intersection.js
+++ src/intersection/intersection.js
@@ -8,10 +8,8 @@
  */
 
 export const intersection = (array = [], values = []) => {
-  if (typeof array !== 'object' || typeof values !== 'object')
-    throw new TypeError('arguments should be arrays');
-  if (array === null || values === null)
-    throw new Error('arguments should be arrays');
+  if (typeof array !== 'object' || typeof values !== 'object') { throw new TypeError('arguments should be arrays'); }
+  if (array === null || values === null) { throw new Error('arguments should be arrays'); }
   const newArray = [];
   array.forEach((element) => {
     if (!newArray.includes(element)) {
