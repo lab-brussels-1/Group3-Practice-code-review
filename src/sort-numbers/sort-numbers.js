bf6de3793c8840cc46a341080523706155d618a7
Maria <naplesaries2014@gmail.com>
2022-07-14 17:51:33 +0200

    formated code

--- src/sort-numbers/sort-numbers.js
+++ src/sort-numbers/sort-numbers.js
@@ -7,12 +7,11 @@
 
 export const sortNumbers = (arrayOfNumbers = []) => {
   if (
-    typeof arrayOfNumbers !== 'object' ||
-    arrayOfNumbers.includes(NaN) ||
-    arrayOfNumbers.includes(Infinity) ||
-    arrayOfNumbers.includes(-Infinity)
-  )
-    throw new Error('argument is not valid');
+    typeof arrayOfNumbers !== 'object'
+    || arrayOfNumbers.includes(NaN)
+    || arrayOfNumbers.includes(Infinity)
+    || arrayOfNumbers.includes(-Infinity)
+  ) { throw new Error('argument is not valid'); }
   let sortedArray = [...arrayOfNumbers];
   sortedArray = sortedArray.sort((a, b) => {
     return a - b;
