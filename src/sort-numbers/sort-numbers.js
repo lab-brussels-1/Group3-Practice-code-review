ea795b5e724de011b67446bd9c0562cea2af360f
Maria <naplesaries2014@gmail.com>
2022-07-16 17:48:47 +0200

    added solution for sort-numbers

---
+++ src/sort-numbers/sort-numbers.js
@@ -0,0 +1,22 @@
+/**
+ * Sorts an array of numbers from smallest to largest.
+ *
+ * @param {number[]} [arrayOfNumbers=[]] - The array of numbers to sort.
+ * @returns {number[]} A new array with the same numbers, but sorted.
+ */
+
+export const sortNumbers = (arrayOfNumbers = []) => {
+  if (
+    typeof arrayOfNumbers !== 'object'
+    || arrayOfNumbers.includes(NaN)
+    || arrayOfNumbers.includes(Infinity)
+    || arrayOfNumbers.includes(-Infinity)
+  ) {
+    throw new Error('argument is not valid');
+  }
+  let sortedArray = [...arrayOfNumbers];
+  sortedArray = sortedArray.sort((a, b) => {
+    return a - b;
+  });
+  return sortedArray;
+};
