b73a42c04a2af213db1180c01cfabf9d7fcfa2fc
minju <minjupgeorge@gmail.com>
2022-07-19 18:32:55 +0200

    file name edited

--- src/sort-numbers/example-built-in-sort/sort-numbers.spec.js
+++ src/sort-numbers/example-built-in-sort/sort-numbers.spec.js
@@ -1,68 +1,52 @@
-import { sortNumbers } from './sort-numbers.js';
+import { sortNumbers } from './sort-number.js';
 
-describe('sortNumbers sorts an array of numbers', () => {
-  describe('sorts positive numbers', () => {
-    it('whole numbers', () => {
-      const expected = [0, 1, 2, 3, 5];
-      const received = sortNumbers([5, 2, 0, 3, 1]);
-      expect(received).toEqual(expected);
-    });
-    it('decimal numbers', () => {
-      const expected = [0, 1.11, 2.01, 3.8, 5.4];
-      const received = sortNumbers([5.4, 2.01, 3.8, 0, 1.11]);
-      expect(received).toEqual(expected);
-    });
-    it('mixed whole and decimal numbers', () => {
-      const expected = [1, 2, 3.8, 5.4];
-      const received = sortNumbers([5.4, 2, 3.8, 1]);
-      expect(received).toEqual(expected);
-    });
+describe('Sorts an array of numbers from smallest to largest', () => {
+  it('integer numbers', () => {
+    expect(sortNumbers([10, 0, 9, -1, 3])).toEqual([-1, 0, 3, 9, 10]);
   });
-  describe('sorts negative numbers', () => {
-    it('whole numbers', () => {
-      const expected = [-5, -3, -2, -1, 0];
-      const received = sortNumbers([-1, 0, -3, -2, -5]);
-      expect(received).toEqual(expected);
-    });
-    it('decimal numbers', () => {
-      const expected = [-5.09, -3.01, -2.56, -1.4, 0];
-      const received = sortNumbers([-1.4, -3.01, -2.56, 0, -5.09]);
-      expect(received).toEqual(expected);
-    });
-    it('mixed whole and decimal numbers', () => {
-      const expected = [-5.09, -3.01, -2, -1, 0];
-      const received = sortNumbers([0, -1, -3.01, -2, -5.09]);
-      expect(received).toEqual(expected);
-    });
+  it('decimal numbers', () => {
+    expect(sortNumbers([0.005, 0, 0.7, 0.01])).toEqual([0, 0.005, 0.01, 0.7]);
   });
-  describe('sorts mixed positive and negative numbers', () => {
-    it('whole numbers', () => {
-      const expected = [-2, -1, 0, 1, 2];
-      const received = sortNumbers([0, 1, -1, 2, -2]);
-      expect(received).toEqual(expected);
-    });
-    it('decimal numbers', () => {
-      const expected = [-2.34, -1.01, 0, 1.4, 2.99];
-      const received = sortNumbers([0, 1.4, -1.01, 2.99, -2.34]);
-      expect(received).toEqual(expected);
-    });
-    it('mixed whole and decimal numbers', () => {
-      const expected = [-5.09, -1, 0, 2, 3.01];
-      const received = sortNumbers([-1, 3.01, 0, 2, -5.09]);
-      expect(received).toEqual(expected);
-    });
+  it('mixed whole and decimal numbers', () => {
+    const expected = [1, 2, 3.8, 5.4];
+    const received = sortNumbers([5.4, 2, 3.8, 1]);
+    expect(received).toEqual(expected);
   });
-  describe('has no side-effects', () => {
-    it('returns a new array', () => {
-      const arg = [];
-      const returned = sortNumbers(arg);
-      const areDifferent = arg !== returned;
-      expect(areDifferent).toEqual(true);
-    });
-    it('does not modify the argument', () => {
-      const arg = [3, 2, 1];
-      sortNumbers(arg);
-      expect(arg).toEqual([3, 2, 1]);
-    });
+  it('only negative numbers', () => {
+    expect(sortNumbers([-1000, -0.002, -1, -76])).toEqual([
+      -1000, -76, -1, -0.002,
+    ]);
+  });
+  it('sorted numbers should stay in the same order', () => {
+    expect(sortNumbers([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
+});
+describe('There is no side-effect', () => {
+  it('Returns a new array without modifying the original array.', () => {
+    const array = [10, 5, 8, 8, 9];
+    expect(sortNumbers(array) === array).toEqual(false);
+  });
+});
+describe('The array is not of numbers', () => {
+  it('When no argument passed a default value should be applied', () => {
+    expect(sortNumbers()).toEqual([]);
+  });
+  it('When argument is an empty array function should return an empty array', () => {
+    expect(sortNumbers([])).toEqual([]);
+  });
+  it('When argument is NaN function should raise an error ', () => {
+    expect(() => sortNumbers(NaN)).toThrow('argument is not valid');
+  });
+  it('When array includes NaN function should raise an error ', () => {
+    expect(() => sortNumbers([NaN, 1, 4])).toThrow('argument is not valid');
+  });
+  it('[Infinity]--> function should raise an error', () => {
+    expect(() => sortNumbers([Infinity])).toThrow('argument is not valid');
+  });
+  it('[-Infinity]--> function should raise an error', () => {
+    expect(() => sortNumbers([-Infinity])).toThrow('argument is not valid');
+  });
+  it('["3","2","1"]--> function should raise an error', () => {
+    expect(sortNumbers(['3', '2', '1'])).toEqual(['1', '2', '3']);
   });
 });
