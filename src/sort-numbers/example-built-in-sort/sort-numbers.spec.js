bae16aec347ef6bd71b4faa6bebbb27bf437da07
minju <minjupgeorge@gmail.com>
2022-07-21 12:38:58 +0200

    test files corrected

--- src/sort-numbers/example-built-in-sort/sort-numbers.spec.js
+++ src/sort-numbers/example-built-in-sort/sort-numbers.spec.js
@@ -66,5 +66,3 @@ describe('sortNumbers sorts an array of numbers', () => {
     });
   });
 });
-
-
--- src/sort-numbers/sort-numbers.spec.js
+++ src/sort-numbers/sort-numbers.spec.js
@@ -66,4 +66,3 @@ describe('sortNumbers sorts an array of numbers', () => {
     });
   });
 });
-
