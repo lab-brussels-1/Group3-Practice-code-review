63bd3e26336a23f29fcfc7857778d1f60ccf6c8f
alexanderism55 <99040483+alexanderism55@users.noreply.github.com>
2022-07-23 18:51:21 +0200

    linted the js

--- src/deep-flat/alex-deep-flat/alex-deep-flat.js
+++ src/deep-flat/alex-deep-flat/alex-deep-flat.js
@@ -1,6 +1,6 @@
-export function flatter(array) {
-  // I used the property flat and gave it a depth of infinity so that the arraay is flattened regadless of its debth
-  let result = array.flat([Infinity]);
+export const flatter = (array) => {
+  // I used the property flat and gave it a depth of infity
+  const result = array.flat([Infinity]);
   // I return result for the function
   return result;
-}
+};
--- src/difference/alex-difference/alex-difference.js
+++ src/difference/alex-difference/alex-difference.js
@@ -1,14 +1,3 @@
-//export function difference(array1, array2) {
-//let result = [];
-//for (let i = 0; i < array1.length; i++) {
-//for (let j = i; j < array2.length; j++) {
-//if (array1[i] !== array2[i]) {
-//result.push(array1[i]);
-//}
-//}
-//return result;
-//}
-//}
 export const difference = (array = [], values = []) => {
   const newArray = [];
   if (typeof array !== 'object' || typeof values !== 'object') {
